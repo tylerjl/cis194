0e9f23fda667d0bc111a3e39cabfa604df2e3746
Tyler Langlois <tjl@byu.net>
2015-04-04 00:37:32 -0600

    hw07: start homework set and complete exercise 1

---
+++ hw07/JoinList.hs
@@ -0,0 +1,17 @@
+module JoinList where
+
+import Data.Monoid
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+    deriving (Eq, Show)
+
+-- Exercise 1
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) a b = Append (tag a <> tag b) a b
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = mempty
+tag (Single m _)   = m
+tag (Append m _ _) = m

7b6b2fd5f8186f0479fdbf925085cd61e1593203
Tyler Langlois <tjl@byu.net>
2015-04-08 21:31:49 -0600

    HW7: complete exercise 1

--- hw08/Party.hs
+++ hw08/Party.hs
@@ -1,5 +1,7 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Party where
 
+import Data.Monoid
 import Employee
 
 -- Exercise 1
@@ -8,3 +10,12 @@ import Employee
 glCons :: Employee -> GuestList -> GuestList
 glCons e@(Emp {empFun = f}) (GL [] _)  = GL [e] f
 glCons e@(Emp {empFun = f}) (GL xs fc) = GL (e:xs) (f+fc)
+
+-- .2
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL xs f1) (GL ys f2) = GL (xs++ys) (f1+f2)
+
+-- .3
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max

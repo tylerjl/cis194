d826c46fe6f20e27d0a54b74f45b595483339e6d
Tyler Langlois <tjl@byu.net>
2015-04-10 23:09:10 -0600

    Complete homework 8

--- hw08/Party.hs
+++ hw08/Party.hs
@@ -1,7 +1,9 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Party where
 
 import Data.Monoid
+import Data.Tree
 import Employee
 
 -- Exercise 1
@@ -19,3 +21,30 @@ instance Monoid GuestList where
 -- .3
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
+
+-- Exercise 2
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f (Node {rootLabel = l, subForest = ns}) =
+    f l (map (treeFold f) ns)
+
+-- Exercise 3
+nextLevel :: Employee ->
+             [(GuestList, GuestList)] ->
+              (GuestList, GuestList)
+nextLevel boss gLists= (bestWith, bestWithout)
+    where bestWith    = glCons boss . mconcat . map fst $ gLists
+          bestWithout = mconcat . map (uncurry moreFun) $ gLists
+
+-- Exercise 4
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry moreFun . treeFold nextLevel
+
+-- Exercise 5
+main :: IO ()
+main = do
+     company <- readFile "company.txt"
+     putStrLn . printGL $ maxFun (read company :: Tree Employee)
+
+printGL :: GuestList -> String
+printGL (GL es fun) = "Total fun: " ++ show fun ++ '\n' : employees
+    where employees = unlines . map empName $ es

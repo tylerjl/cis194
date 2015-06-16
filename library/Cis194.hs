b9244f7a3dad0e7b993d54d4a4edc56a48a1a055
Tyler Langlois <tjl@byu.net>
2015-06-15 23:11:22 -0600

    Cleanup

--- library/Cis194.hs
+++ library/Cis194.hs
@@ -1,12 +1,4 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 -- | Top-level namespace for CIS194 exercises.
-module Cis194 (module Cis194) where
-
--- | Boilerplate main.
-main :: IO ()
-main = print cis194
-
--- | I have no idea what I'm doing :doge:
-cis194 :: ()
-cis194 = ()
+module Cis194 where
--- test-suite/Cis194Spec.hs
+++ test-suite/Cis194Spec.hs
@@ -1,15 +1,9 @@
 module Cis194Spec (spec) where
 
-import Cis194
-
 import Test.Hspec
-import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec =
-    describe "cis194" $ do
-        it "returns the unit" $
-            cis194 `shouldBe` ()
-
-        prop "equals the unit value" $
-            \ x -> cis194 == x
+    describe "Cis194" $ do
+        it "encapsulates the course" $
+            pending

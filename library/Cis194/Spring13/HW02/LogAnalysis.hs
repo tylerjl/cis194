bed85fc1cc72e073229cc35c09bbdd83c3372aef
Tyler Langlois <tjl@byu.net>
2015-06-22 23:06:42 -0600

    hw02: implement parseMessage in applicative parsec style

--- library/Cis194/Spring13/HW02/LogAnalysis.hs
+++ library/Cis194/Spring13/HW02/LogAnalysis.hs
@@ -4,15 +4,35 @@ module Cis194.Spring13.HW02.LogAnalysis
     ( parseMessage
 ) where
 
+import Control.Applicative ((<|>))
+import Text.Parsec (eof, parse, many1, manyTill, ParseError)
+import Text.Parsec.Char (anyChar, char, space, digit)
+import Text.Parsec.String (Parser)
 import Cis194.Spring13.HW02.Log
 
+-- |Parse a LogMessage from a raw string.
 parseMessage :: String -> LogMessage
-parseMessage m@([]) = Unknown m
-parseMessage m@(c:cs)
-    | c == 'I' = LogMessage Info 0 cs
-    | otherwise = Unknown m
+parseMessage s =
+    case regularParse parseLog s of
+         Right p -> p
+         Left  _ -> Unknown s
 
--- parseTS :: String -> Maybe Int
--- parseTS m = case matchRegex pattern m of
---                  Just i -> 
---     where pattern = mkRegex " +(\d+) +"
+-- |Generic parsing wrapper
+regularParse :: Parser a -> String -> Either ParseError a
+regularParse p = parse p ""
+
+-- |High-level parser for the log as a whole
+parseLog :: Parser LogMessage
+parseLog = LogMessage <$> (logType <* space)
+                      <*> (timeStamp <* space)
+                      <*> (manyTill anyChar eof)
+
+-- |Parse the log type
+logType :: Parser MessageType
+logType = Info             <$  (char 'I')
+      <|> Warning          <$  (char 'W')
+      <|> Error <$> (read) <$> (char 'E' *> many1 space *> many1 digit)
+
+-- |Parse message timestamp
+timeStamp :: Parser TimeStamp
+timeStamp = read <$> many1 digit

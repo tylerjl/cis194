dedc115c7cec59fa227e528e0571f76b997a7b4a
Tyler Langlois <tjl@byu.net>
2015-06-23 22:30:26 -0600

    hw02: working spec for whatWentWrong

--- library/Cis194/Spring13/HW02/LogAnalysis.hs
+++ library/Cis194/Spring13/HW02/LogAnalysis.hs
@@ -1,14 +1,25 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Cis194.Spring13.HW02.LogAnalysis
-    ( parseMessage
+    ( parse
+    , parseMessage
+    , insert
+    , build
+    , inOrder
+    , whatWentWrong
 ) where
 
+import Data.Monoid ((<>))
+import Data.List (foldl')
 import Control.Applicative ((<|>))
-import Text.Parsec (eof, parse, many1, manyTill, ParseError)
+import qualified Text.Parsec as P
 import Text.Parsec.Char (anyChar, char, space, digit)
 import Text.Parsec.String (Parser)
 import Cis194.Spring13.HW02.Log
+
+-- |Parse [LogMessage] from a raw string
+parse :: String -> [LogMessage]
+parse = fmap (parseMessage) . lines
 
 -- |Parse a LogMessage from a raw string.
 parseMessage :: String -> LogMessage
@@ -18,21 +29,61 @@ parseMessage s =
          Left  _ -> Unknown s
 
 -- |Generic parsing wrapper
-regularParse :: Parser a -> String -> Either ParseError a
-regularParse p = parse p ""
+regularParse :: Parser a -> String -> Either P.ParseError a
+regularParse p = P.parse p ""
 
 -- |High-level parser for the log as a whole
 parseLog :: Parser LogMessage
 parseLog = LogMessage <$> (logType <* space)
                       <*> (timeStamp <* space)
-                      <*> (manyTill anyChar eof)
+                      <*> (P.manyTill anyChar P.eof)
 
 -- |Parse the log type
 logType :: Parser MessageType
 logType = Info             <$  (char 'I')
       <|> Warning          <$  (char 'W')
-      <|> Error <$> (read) <$> (char 'E' *> many1 space *> many1 digit)
+      <|> Error <$> (read) <$> (char 'E' *> P.many1 space *> P.many1 digit)
 
 -- |Parse message timestamp
 timeStamp :: Parser TimeStamp
-timeStamp = read <$> many1 digit
+timeStamp = read <$> P.many1 digit
+
+{-
+ - Insert LogMessage into MessageTree, return resultant MessageTree.
+ - Tree should be sorted as a binary search tree.
+ - Unknown LogMessage arguments should be ignored, returning the original
+ - MessageTree.
+ -}
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t                = t
+insert m (Leaf)                     = Node Leaf m Leaf
+insert n (Node lt m@(Unknown _) rt) = Node lt m (insert n rt)
+insert n@(LogMessage _ ts _) (Node lt m@(LogMessage _ v _) rt)
+    | v < ts    = Node lt m (insert n rt)
+    | otherwise = Node (insert n lt) m rt
+
+-- |Construct a MessageTree from [LogMessage]
+build :: [LogMessage] -> MessageTree
+build = foldl' (flip insert) Leaf
+
+-- |Traverse a MessageTree in-order to return a sorted [LogMessage]
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Leaf)       = []
+inOrder (Node l v r) = inOrder l <> [v] <> inOrder r
+
+-- |Returns list of [String] with all error log messages with severity 50
+-- |or greater sorted by timestamp.
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = fmap (msgBody) . inOrder . build . filter (isCritical)
+
+-- |Print the message body of a LogMessage
+msgBody :: LogMessage -> String
+msgBody (Unknown b)        = b
+msgBody (LogMessage _ _ b) = b
+
+-- |Determine whether a LogMessage is an Error
+isCritical :: LogMessage -> Bool
+isCritical (LogMessage (Error s) _ _)
+    | s >= 50   = True
+    | otherwise = False
+isCritical _ = False
--- test-suite/Cis194/Spring13/HW02Spec.hs
+++ test-suite/Cis194/Spring13/HW02Spec.hs
@@ -5,6 +5,36 @@ import Cis194.Spring13.HW02.LogAnalysis
 
 import Test.Hspec
 
+oneTree :: MessageTree
+oneTree = (Node
+    Leaf
+    (LogMessage (Error 5) 10 "zero")
+    Leaf)
+
+twoTree :: MessageTree
+twoTree = (Node
+    Leaf
+    (LogMessage Info 100 "zero")
+    (Node
+        Leaf
+        (LogMessage Warning 150 "one")
+        Leaf))
+
+threeTree :: MessageTree
+threeTree = (Node
+    (Node
+        Leaf
+        (LogMessage (Error 1) 50 "zero")
+        Leaf)
+    (LogMessage Info 100 "one")
+    (Node
+        Leaf
+        (LogMessage Warning 150 "two")
+        Leaf))
+
+trees :: [MessageTree]
+trees = [oneTree, twoTree, threeTree]
+
 spec :: Spec
 spec =
     describe "HW02" $ do
@@ -20,3 +50,69 @@ spec =
             it "parses unknowns" $
                 parseMessage "This is not in the right format"
                     `shouldBe` Unknown "This is not in the right format"
+
+        describe "insert" $ do
+            it "should not insert Unknown messages" $
+                map (\t -> insert (Unknown "test") t) trees `shouldBe` trees
+
+            it "should insert earlier messages to the left" $
+                insert (LogMessage Info 5 "info") oneTree `shouldBe`
+                    (Node
+                    (Node
+                        Leaf
+                        (LogMessage Info 5 "info")
+                        Leaf)
+                    (LogMessage (Error 5) 10 "zero")
+                    Leaf)
+
+            it "should insert later messages to the right" $
+                insert (LogMessage (Error 10) 125 "error") threeTree `shouldBe`
+                    (Node
+                        (Node
+                            Leaf
+                            (LogMessage (Error 1) 50 "zero")
+                            Leaf)
+                        (LogMessage Info 100 "one")
+                        (Node
+                            (Node
+                                Leaf
+                                (LogMessage (Error 10) 125 "error")
+                                Leaf)
+                            (LogMessage Warning 150 "two")
+                            Leaf))
+
+            it "should insert into empty trees" $
+                insert (LogMessage Warning 100 "warning") Leaf `shouldBe`
+                    (Node Leaf (LogMessage Warning 100 "warning") Leaf)
+
+        describe "build" $ do
+            it "should create single-node trees" $
+                build [(LogMessage (Error 5) 10 "zero")] `shouldBe`
+                    oneTree
+
+            it "should create two-node trees" $
+                build [(LogMessage Info 100 "zero"),
+                       (LogMessage Warning 150 "one")]
+                      `shouldBe` twoTree
+
+            it "should create three-node trees" $
+                build [(LogMessage Info 100 "one"),
+                       (LogMessage (Error 1) 50 "zero"),
+                       (LogMessage Warning 150 "two")]
+                      `shouldBe` threeTree
+
+        describe "inOrder" $ do
+            it "should print single-node trees in order" $
+                inOrder oneTree `shouldBe`
+                    [(LogMessage (Error 5) 10 "zero")]
+
+            it "should print two-node trees in order" $
+                inOrder twoTree `shouldBe`
+                    [(LogMessage Info 100 "zero"),
+                     (LogMessage Warning 150 "one")]
+
+            it "should print three-node trees in order" $
+                inOrder threeTree `shouldBe`
+                    [(LogMessage (Error 1) 50 "zero"),
+                     (LogMessage Info 100 "one"),
+                     (LogMessage Warning 150 "two")]
